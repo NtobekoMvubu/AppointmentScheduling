444081546d79191a8bf21092319f83d71401ec46
Ntobeko Mvubu <ntobekoj7@gmail.com>
2022-02-13 06:58:38 +0200

    Added the Login and Register features. Added roles feature as well. Moved the applicationDbContext from Data folder to the models folder. added a partial view to display the options on the nav bar which depend if the user is login in or nor

--- AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using AppointmentScheduling.Data;
+﻿
+using AppointmentScheduling.Models;
+using AppointmentScheduling.Models.ViewModels;
+using AppointmentScheduling.Utility;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,17 +15,91 @@ namespace AppointmentScheduling.Controllers
     {
 
         private readonly ApplicationDbContext _db;
-        public AccountController(ApplicationDbContext db)
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
+
         {
             _db = db;
-        }
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
 
+        }
+        //GET Login
         public IActionResult Login()
         {
             return View();
         }
 
+        //POST-login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalide Login Attempt");
+            }
+            return View();
+        }
 
+
+        //GET-Register 
+        public async Task<IActionResult> Register()
+        {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Doctor));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Patient));
+            }
+            return View();
+        }
+
+        //POST-CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register (RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
 
     }
 }
